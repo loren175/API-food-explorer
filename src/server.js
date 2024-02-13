907d38061b944f449ae52888897955f05fd27334
Rafael Loren175 <rafael.loren175@gmail.com>
2024-02-13 14:33:55 -0300

    add database config and create user feature

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require("express-async-errors")
-const database = require("./database/sqlite")
+const migrationsRun = require("./database/sqlite/migrations")
 
 const AppError = require("./utils/AppError")
 const express = require("express")
@@ -11,7 +11,7 @@ app.use(express.json())
 
 app.use(routes)
 
-database()
+migrationsRun()
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
