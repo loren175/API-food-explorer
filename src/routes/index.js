27453281e5435a95705e7042900480a505c9a5f2
Rafael Loren175 <rafael.loren175@gmail.com>
2024-02-14 15:50:55 -0300

    criate sessions controller, routes and add validation to user and jsonwebtoken

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require("express")
 
 const  usersRouter = require("./users.routes")
 const  dishesRouter = require("./dishes.routes")
+const  sessionsRouter = require("./sessions.routes")
 
 const routes = Router()
 
 routes.use("/users", usersRouter)
 routes.use("/dish", dishesRouter)
+routes.use("/sessions", sessionsRouter)
 
 module.exports = routes
