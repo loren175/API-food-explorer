a96628a16694d1fb7554fda3561c93d55ddd7896
Rafael Loren175 <rafael.loren175@gmail.com>
2024-02-14 10:34:50 -0300

    create dishes controller and routes and adjusted dishes migration

--- src/database/knex/migrations/20240213175716_createDish.js
+++ src/database/knex/migrations/20240213175716_createDish.js
@@ -1,11 +1,11 @@
 exports.up = (knex) =>
   knex.schema.createTable("dishes", (table) => {
     table.increments("id")
-    table.text("title")
+    table.text("name")
     table.text("description")
     table.text("image").default(null)
+    table.text("category").notNullable()
     table.decimal("price", 10, 2).notNullable()
-    table.integer("user_id").references("id").inTable("users")
 
     table.integer("created_by").references("id").inTable("users")
     table.integer("updated_by").references("id").inTable("users")
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express")
 
 const  usersRouter = require("./users.routes")
+const  dishesRouter = require("./dishes.routes")
 
 const routes = Router()
 
 routes.use("/users", usersRouter)
+routes.use("/dish", dishesRouter)
 
 module.exports = routes
