ec6ec4e2ddb48b690244e870ea5ef726e2715fa3
Rafael Loren175 <rafael.loren175@gmail.com>
2024-02-14 16:38:31 -0300

    add middleware to dishes

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,10 +1,14 @@
 const { Router } = require("express")
 const DishesController = require("../controllers/DishesController")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+
 const dishesRoutes = Router()
 
 const dishesController = new DishesController()
 
-dishesRoutes.post("/:user_id", dishesController.create)
+dishesRoutes.use(ensureAuthenticated)
+
+dishesRoutes.post("/", dishesController.create)
 dishesRoutes.get("/", dishesController.index)
 dishesRoutes.get("/:id", dishesController.show)
 dishesRoutes.delete("/:id", dishesController.delete)
