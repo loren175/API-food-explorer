167d5e36e0ec9a2966b60709fe87d5be100cd49c
Rafael Loren175 <rafael.loren175@gmail.com>
2024-02-14 10:46:58 -0300

    add delete method

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -36,6 +36,14 @@ class DishesController {
 
     return response.json({ ...dish, ingredients })
   }
+
+  async delete(request, response) {
+    const { id } = request.params
+    
+    await knex("dishes").where({ id }).delete()
+
+    return response.json()
+  }
 }
 
 module.exports = DishesController
