37fd75901180e7cba8517b3b1f9878789a51e8b6
Rafael Loren175 <rafael.loren175@gmail.com>
2024-02-20 17:45:48 -0300

    add all dishes and add time to cookie token

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -30,7 +30,7 @@ class SessionsController {
       httpOnly: true,
       sameSite: "none",
       secure: true,
-      maxAge: 15 * 60 * 1000,
+      maxAge: 60 * 60 * 1000,
     })
 
     delete user.password
