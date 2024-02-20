88098a613c5aa5c4c43e74be32ad3169fa876d3a
Rafael Loren175 <rafael.loren175@gmail.com>
2024-02-20 09:00:53 -0300

    add cookies and cookie parser lib

--- src/server.js
+++ src/server.js
@@ -4,12 +4,18 @@ const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
 const UploadConfig = require("./configs/upload")
 
+const cookieParser = require("cookie-parser")
+
 const cors = require("cors")
 const express = require("express")
 const routes = require("./routes")
 
 const app = express()
-app.use(cors())
+app.use(cookieParser())
+app.use(cors({
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  credentials: true
+}))
 app.use(express.json())
 
 app.use("/files", express.static(UploadConfig.UPLOADS_FOLDER))
